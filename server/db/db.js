b0d4daea7852a7544e84c9d0a11eaff9a2810de4
nolastemgarden <nolastemgarden@gmail.com>
2022-01-15 14:14:23 -0600

    Fix stub of db initialization, Sequelize localhost connection.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,17 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+// const db = new Sequelize("sqlite::memory:");
+// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+//   logging: false
+// });
+
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//     host: 'localhost',
+//     dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+// });
+const db = new Sequelize('messenger', 'postgres', 'scioto7', {
+    host: 'localhost',
+    dialect: 'postgres' 
 });
 
 module.exports = db;
