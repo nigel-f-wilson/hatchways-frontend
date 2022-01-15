53ee77aad4fcab78cf8a99ff2817a2d76be58f1f
nolastemgarden <nolastemgarden@gmail.com>
2022-01-15 17:14:59 -0600

    Revert "Fix stub of db initialization, Sequelize localhost connection."
    This reverts commit b0d4daea7852a7544e84c9d0a11eaff9a2810de4.

--- server/db/db.js
+++ server/db/db.js
@@ -1,17 +1,7 @@
 const Sequelize = require("sequelize");
 
-// const db = new Sequelize("sqlite::memory:");
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-//   logging: false
-// });
-
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//     host: 'localhost',
-//     dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-// });
-const db = new Sequelize('messenger', 'postgres', 'scioto7', {
-    host: 'localhost',
-    dialect: 'postgres' 
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
 
 module.exports = db;
