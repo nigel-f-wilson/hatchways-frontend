1401547302238eeab5a312a39e324bb0b538a0f7
nolastemgarden <nolastemgarden@gmail.com>
2022-01-22 17:26:27 -0600

    Revert db to before setting up for localhost
    I'm switching which one of two code blocks is commented out vs active. This commit returns the db to it's status in the starter code. I'm removing changes I made to get it to connect to my locally hosted postgres db.

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-//   logging: false
-// });
-const db = new Sequelize('messenger', 'postgres', 'scioto7', {
-  host: 'localhost',
-  dialect: 'postgres'
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
+// const db = new Sequelize('messenger', 'postgres', 'scioto7', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
 
 module.exports = db;
