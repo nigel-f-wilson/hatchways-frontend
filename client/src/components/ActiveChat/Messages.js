ccf9bda5fd247588d82dc4c5cc7c5b3cfa2c2f8a
nolastemgarden <nolastemgarden@gmail.com>
2022-01-22 16:03:09 -0600

    Move Message array sorting up to ActiveChat
    Remove variable reversed messages because the errors in sorting were more than mere reversal.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,14 +5,10 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-
-  const reversedMessages = messages.slice().reverse()
-
   return (
     <Box>
-      {reversedMessages.map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
-
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
