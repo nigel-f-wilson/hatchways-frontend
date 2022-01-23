088f2cc1cafd4e2e98184c719e6275d452622070
nolastemgarden <nolastemgarden@gmail.com>
2022-01-23 15:28:45 -0600

    Add overrides for typography and Buttons

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -4,6 +4,15 @@ export const theme = createMuiTheme({
   typography: {
     fontFamily: "Open Sans, sans-serif",
     fontSize: 14,
+    h1: {
+      fontWeight: "400",
+      fontSize: "26px",
+      lineHeight: "40px"
+    },
+    h2: {
+      letterSpacing: 0,
+      fontWeight: "bold"
+    },
     button: {
       textTransform: "none",
       letterSpacing: 0,
@@ -15,10 +24,33 @@ export const theme = createMuiTheme({
       input: {
         fontWeight: "bold"
       }
+    },
+    MuiButtonBase: {
+      root: {
+        boxSizing: "border-box",
+      },
+    },
+    MuiButton: {
+      root: {
+        boxSizing: "border-box",
+      },
+      contained: {
+        height: "100%",
+        margin: 0,
+        "&:hover": {
+          border: "solid 2px",
+          backgroundColor: "#FFF"
+        }
+      }
+
+      
     }
   },
   palette: {
     primary: { main: "#3A8DFF" },
-    secondary: { main: "#B0B0B0" }
+    secondary: { 
+      main: "#B0B0B0",
+      contrastText: "#fff"
+    }
   }
 });
