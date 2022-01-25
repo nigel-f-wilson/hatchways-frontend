8c708abf1885de184842b35db05f7a3a1dfb735d
nolastemgarden <nolastemgarden@gmail.com>
2022-01-25 16:57:41 -0600

    Shorten compareDates to a one line function
    By converting JS Date objects instead of comparing in their original string formats

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -20,27 +20,13 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-// Need to write comparator for array.sort to use that handles date-times
-// Testing data:
-// let earlierDate = "2022-01-21T01:07:54.777Z"
-// let laterDate = "2022-01-21T01:07:54.888Z"
-function compareDates(dateOne,dateTwo) {
-  console.assert(typeof(dateOne) === "string" && typeof(dateTwo) === "string");
-  for (let i = 0; i < dateOne.length; i++) {
-    if (dateOne[i] < dateTwo[i]) {
-      return -1
-    }
-    if (dateOne[i] > dateTwo[i]) {
-      return 1
-    }
-  }
-  return 0
-}
+const compareDates = (dateOne, dateTwo) => new Date(dateOne) - new Date(dateTwo)
+
 const ActiveChat = (props) => {
   const classes = useStyles();
   const { user } = props;
   const conversation = props.conversation || {};
-
+  
   const messages = (conversation.messages) ? [...conversation.messages].sort((a, b) => compareDates(a.createdAt, b.createdAt)) : []
   return (
     <Box className={classes.root}>
