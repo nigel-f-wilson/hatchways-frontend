94d69aee1a9bf9ae2c6aabc25926274228281909
nolastemgarden <nolastemgarden@gmail.com>
2022-01-26 18:26:05 -0600

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -3,9 +3,6 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
-// const db = new Sequelize('messenger', 'postgres', 'scioto7', {
-//   host: 'localhost',
-//   dialect: 'postgres'
-// });
+
 
 module.exports = db;
