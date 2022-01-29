e30d522df62d816cdd4cb9f52d64edbb3cd8b5eb
nolastemgarden <nolastemgarden@gmail.com>
2022-01-29 12:49:05 -0600

    Build Mobile version of Signup page
    Combine the new Landing Page that has conditional rendering based on screen width with the Old Signup page that was styled but only for desktop.

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { Grid, Box, Paper } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
@@ -9,16 +9,24 @@ import WelcomeHeader from "./components/LoginAndSignup/WelcomeHeader";
 import { LinkToLogin } from "./components/LoginAndSignup/Navigation";
 import { SignupForm } from "./components/LoginAndSignup/Forms";
 
+import { useScreenWidth } from "./hooks";
+
+
 const useStyles = makeStyles((theme) => ({
-  root: {
+  desktopRoot: {
     overflow: "hidden",
     height: "100vh",
     display: "flex",
     alignItems: "center",
   },
-  formArea: {
-    height: "60vh",
-    minHeight: "400px"
+  desktopSidebar: {
+    height: "100vh",
+  },
+  desktopFormArea: {
+    height: "40vh",
+    minHeight: "300px",
+    minWidth: "380px",
+    paddingRight: "5%"
   },
   topRight: {
     height: "60px",
@@ -26,12 +34,37 @@ const useStyles = makeStyles((theme) => ({
     top: "2rem",
     right: "3rem",
   },
+  mobileRoot: {
+    width: "100vw",
+    minHeight: "850px",
+    overflow: "scroll",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: theme.palette.secondary.main,
+    position: "relative",
+  },
+  mobileHeader: {
+    width: "100vw",
+    height: "100vh",
+  },
+  mobilePaper: {
+    margin: "-20vh 0 3rem",
+    width: "90%",
+    maxWidth: "600px",
+    padding: "1.5rem",
+    height: "auto",
+    minHeight: "450px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 }));
 
 const Signup = (props) => {
   const classes = useStyles();
-  const history = useHistory();
-
+  const width = useScreenWidth()
   const { user, register } = props;
 
   const [formErrorMessage, setFormErrorMessage] = useState({});
@@ -55,23 +88,46 @@ const Signup = (props) => {
     return <Redirect to="/home" />;
   }
 
-  return (
-    <Grid container className={classes.root}  >
-      <Grid item xs={5} >
-        <WelcomeHeader />
+  if (width > 850) {
+    return <DesktopLayout />
+  } else {
+    return <MobileLayout />
+  }
+
+  function DesktopLayout() {
+    return (
+      <Grid container className={classes.desktopRoot} >
+        <Grid item xs={5} className={classes.desktopSidebar} >
+          <WelcomeHeader />
+        </Grid>
+        <Grid item xs={1} md={false} />
+        <Grid item xs={5} className={classes.desktopFormArea} >
+          <SignupForm
+            handleRegister={handleRegister}
+            formErrorMessage={formErrorMessage}
+          />
+        </Grid>
+        <Box className={classes.topRight} >
+          <LinkToLogin />
+        </Box>
       </Grid>
-      <Grid item xs={1} />
-      <Grid item xs={5} className={classes.formArea} >
-        <SignupForm 
-          handleRegister={handleRegister} 
-          formErrorMessage={formErrorMessage}
-        />
-      </Grid>
-      <Box className={classes.topRight} >
-        <LinkToLogin />
+    );
+  }
+
+  function MobileLayout() {
+    return (
+      <Box container className={classes.mobileRoot} >
+        <WelcomeHeader className={classes.mobileHeader} />
+        <Paper className={classes.mobilePaper} >
+          <SignupForm
+            handleRegister={handleRegister}
+            formErrorMessage={formErrorMessage}
+          />
+          <LinkToLogin />
+        </Paper>
       </Box>
-    </Grid>
-  );
+    );
+  }
 };
 
 const mapStateToProps = (state) => {
