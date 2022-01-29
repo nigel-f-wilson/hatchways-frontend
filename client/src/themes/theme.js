354e38cdd6e58e62f8826de7cd0130abf2b6d46a
nolastemgarden <nolastemgarden@gmail.com>
2022-01-29 07:31:07 -0600

    Update theme.js

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,53 +1,69 @@
 import { createTheme } from "@material-ui/core";
 
 export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 800,
+      lg: 1200,
+      xl: 1536
+    }
+  },  
   typography: {
     fontFamily: "Open Sans, sans-serif",
-    fontSize: 14,
+    fontSize: "14px",
     h1: {
       fontWeight: "400",
       fontSize: "26px",
       lineHeight: "40px"
     },
     h2: {
+      fontSize: "26px",
+
       letterSpacing: 0,
       fontWeight: "bold"
     },
     button: {
       textTransform: "none",
-      letterSpacing: 0,
-      fontWeight: "bold"
+      letterSpacing: 0.5,
+      fontWeight: 600 
     }
   },
   overrides: {
+    Mui: {
+      focused: {
+        color: "#b0b0b0"
+      },
+    },
     MuiInput: {
       input: {
-        fontWeight: "bold"
+        fontWeight: "bold",
+        paddingTop: "20px"
       }
     },
-    MuiButtonBase: {
+    MuiInputLabel: {
       root: {
-        boxSizing: "border-box",
+        color: "#b0b0b0"
+      },
+      focused: {
+        color: "#b0b0b0"
+      },
+      root: {
+        color: "#b0b0b0"
       },
     },
-    MuiButton: {
-      root: {
-        boxSizing: "border-box",
-      },
-      contained: {
-        height: "100%",
-        margin: 0,
-        "&:hover": {
-          border: "solid 2px",
-          backgroundColor: "#FFF"
-        }
-      }
-
-      
-    }
+    
+    
   },
   palette: {
-    primary: { main: "#3A8DFF" },
+    primary: { 
+      light: "#7dbdff",
+      main: "#3A8DFF",
+      dark: "#0061cb",
+      contrastText: "#fff"
+
+    },
     secondary: { 
       main: "#B0B0B0",
       contrastText: "#fff"
