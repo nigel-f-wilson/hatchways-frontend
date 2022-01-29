74412326e9f854152a8adacf1e96646395b6951c
nolastemgarden <nolastemgarden@gmail.com>
2022-01-29 07:23:06 -0600

    Create Navigation.js
    Extract the buttons for navigating between the Login and Signup pages to own file for easy reuse of styling.

---
+++ client/src/components/LoginAndSignup/Navigation.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Box,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    height: "54px",
+  },
+  button: {
+    marginLeft: "2rem",
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.primary.main,
+    height: "100%",
+    width: "170px",
+    border: "solid transparent 2px",  
+    "&:hover": {
+      border: "solid #3A8DFF 2px",  
+      backgroundColor: theme.palette.common.white
+    }
+  },
+}));
+
+export const LinkToSignup = (props) => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  return (
+    <Box className={classes.root} >
+      <Typography
+        color="textSecondary"
+        children="Don't have an account?"
+      />
+      <Button
+        className={classes.button}
+        children="Create account"
+        variant="contained"
+        onClick={() => history.push("/register")}
+      />
+    </Box>
+  );
+};
+
+// export const LinkToLogin = (props) => {
+//   const classes = useStyles();
+//   const history = useHistory();
+
+//   return (
+//     <Box className={classes.root} >
+//       <Typography
+//         color="textSecondary"
+//         children="Already have an account?"
+//       />
+//       <Button
+//         className={classes.button}
+//         children="Login"
+//         variant="contained"
+//         onClick={() => history.push("/login")}
+//       />
+//     </Box>
+//   );
+// };
