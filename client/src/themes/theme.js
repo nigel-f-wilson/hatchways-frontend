80344f4d78fa8e81bdf982d2ef5131c92a076995
nolastemgarden <nolastemgarden@gmail.com>
2022-01-29 10:36:22 -0600

    Revert to default breakpoints

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,15 +1,6 @@
 import { createTheme } from "@material-ui/core";
 
 export const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 800,
-      lg: 1200,
-      xl: 1536
-    }
-  },  
   typography: {
     fontFamily: "Open Sans, sans-serif",
     fontSize: "14px",
@@ -75,3 +66,4 @@ export const theme = createTheme({
     }
   }
 });
+
