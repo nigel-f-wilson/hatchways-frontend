423a93514955a6826b06cc5667e1537dabc972c8
nolastemgarden <nolastemgarden@gmail.com>
2022-02-02 21:49:27 -0600

    Add state for image files

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
 
-import { FormControl, FilledInput, Icon, IconButton } from "@material-ui/core";
+import { FormControl, FilledInput, Icon, IconButton} from "@material-ui/core";
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import ImagePickerDialog from "./ImagePickerDialog";
 
@@ -34,21 +34,26 @@ const Input = (props) => {
   const { postMessage, otherUser, conversationId, user } = props;
 
   const [text, setText] = useState("");
-  const [imagePickerOpen, setImagePickerOpen] = React.useState(false);
-  const [selectedImageFiles, setSelectedImageFiles] = React.useState([]);
+  // const [imagePickerOpen, setImagePickerOpen] = React.useState(false);
 
-  const openImagePicker = () => { setImagePickerOpen(true) };
-  const closeImagePicker = (value) => { 
-    setSelectedImageFiles(value)
-    setImagePickerOpen(false) 
-  };
+  const [selectedImageURLs, setSelectedImageURLs] = useState()  
+  const [selectedImageData, setSelectedImageData] = useState()  
+
+  // const openImagePicker = () => { setImagePickerOpen(true) };
+  // const closeImagePicker = (value) => { 
+  //   selectedImageURLs(value)
+  //   setImagePickerOpen(false) 
+  // };
 
   
   
-  const handleChange = (event) => {
+  const handleTextChange = (event) => {
     setText(event.target.value);
   };
 
+  
+  // this is the original handelSubmit method.
+  // It needs adapted to be able to use with image sender
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -68,6 +73,29 @@ const Input = (props) => {
     setText("");
   };
 
+  function handleImageChange(changeEvent) {
+    const reader = new FileReader();
+
+    reader.onload = function (onLoadEvent) {
+      setSelectedImageURLs(onLoadEvent.target.result);
+      setSelectedImageData(undefined);
+    }
+
+    reader.readAsDataURL(changeEvent.target.files[0]);
+  }
+
+  const handleImageUpload = async (event) => {
+    event.preventDefault();
+
+
+    const form = event.currentTarget;
+    console.log('form', form);
+    // const fileInput = Array.from(form.elements).find(({ name }) => name === 'file');
+    // console.log('fileInput', fileInput);
+
+    
+  };
+
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
       <FormControl hiddenLabel className={classes.row}>
@@ -77,7 +105,7 @@ const Input = (props) => {
           placeholder="Type something..."
           value={text}
           name="text"
-          onChange={handleChange}
+          onChange={handleTextChange}
           
           
           // autoComplete="false"
@@ -88,12 +116,17 @@ const Input = (props) => {
           aria-label="send a photo" 
           onClick={openImagePicker}
         />
-        <ImagePickerDialog 
-          selectedImageFiles={selectedImageFiles}
-          open={imagePickerOpen}
-          onClose={closeImagePicker}
-        />
+        
       </FormControl>
+      {/* <ImagePickerDialog
+        open={imagePickerOpen}
+        onClose={closeImagePicker}
+        selectedImageURLs={selectedImageURLs}
+        selectedImageData={selectedImageData}
+        handleImageChange={handleImageChange}
+        handleImageUpload={handleImageUpload}
+      /> */}
+
     </form>
   );
 };
